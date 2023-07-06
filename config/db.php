334e450e61f6118274395d5ab93038c934f41f65
Jony <jonneyless@163.com>
2023-07-06 13:58:13 +0800

    更改

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\mongodb\Connection',
-    'dsn' => 'mongodb://127.0.0.1:27017/stock_tools?authSource=admin',
+    'dsn' => 'mongodb://jony@www.ijony.com:7520/stock_tools?authSource=admin',
     'options' => [
         'username' => 'jony',
         'password' => '123456',
