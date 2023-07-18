67bb82ce6422c38204f408c2e111ac47c520e264
Jony <jonneyless@163.com>
2023-07-18 18:35:55 +0800

    增加定时任务

--- config/schedule.php
+++ config/schedule.php
@@ -3,4 +3,4 @@
  * @var \omnilight\scheduling\Schedule $schedule
  */
 
-$schedule->command('grab/daily')->dailyAt('17:00')->sendOutputTo(Yii::getAlias('@runtime/logs/schedule.log'));
+$schedule->command('grab/daily')->dailyAt('18:36')->sendOutputTo(Yii::getAlias('@runtime/logs/schedule.log'));
